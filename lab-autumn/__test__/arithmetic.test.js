4bdfb8dfc0725bb0ed2e5b840ff2246e0fab13c7
SeatownAC <33205876+SeatownAC@users.noreply.github.com>
2018-05-20 16:33:39 -0700

    added to arithmetic file

---
+++ lab-autumn/__test__/arithmetic.test.js
@@ -0,0 +1,39 @@
+const arithmetic = require('../lib/arithmetic.js');
+
+describe('adding function', () => {
+
+ it('return the sum of two numbers', () => {
+    let a = 2;
+    let b = 2;
+    let expected = 4;
+    expect(arithmetic.add(a, b)).toBe(expected);
+  });
+
+  it('return null if passed a non number', () => {
+    let str = 'not a number';
+    let a = 2;
+    expect(arithmetic.add(str,a)).toBeNull();
+  });
+
+  it ('return null if passed a boolean', () => {
+      let msg = true;
+      let a = 2;
+      expect(arithmetic.add(msg,a)).toBeNull();
+  });
+});
+
+describe('subtraction function', () => {
+
+  it('return the remainder after the second number is subtracted from first', () => {
+    let a = 2;
+    let b = 2;
+    let expected = 0;
+    expect(arithmetic.sub(a, b)).toBe(expected);
+  });
+
+  it('return null when passed a non number', () => {
+    let str = 'not a number';
+    let b = 2
+    expect(arithmetic.sub(str, b)).toBeNull();
+  });
+});
---
+++ lab-autumn/lib/arithmetic.js
@@ -0,0 +1,19 @@
+module.exports = exports = {};
+
+exports.add = function(x, y) {
+  var sum;
+  if(typeof x !== 'number' || typeof y !== 'number') {
+    return null;
+  }
+  sum = x + y;
+  return sum;
+};
+
+exports.sub = function(x, y) {
+  var difference;
+  if(typeof x !== 'number' || typeof y !== 'number') {
+    return null;
+  }
+  difference = x - y;
+  return difference;
+};
